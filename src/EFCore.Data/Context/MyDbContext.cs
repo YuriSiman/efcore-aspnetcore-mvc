627d63dc361158a70867ea45b28ca3080ecd639e
Yuri Siman <yurisimannogueira@gmail.com>
2021-02-25 19:40:24 -0300

    Criando a classe de Contexto de Dados - MyDbContext

---
+++ src/EFCore.Data/Context/MyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EFCore.Business.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.Data.Context
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MyDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
